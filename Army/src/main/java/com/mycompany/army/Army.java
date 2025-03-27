88133474544b5768f1e38d5cbbc7c880886a0ba2
Холостова Арина <airsha2004@gmail.com>
2025-03-27 10:19:08 +0300

    Creation

---
+++ Army/src/main/java/com/mycompany/army/Army.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.army;
+
+/**
+ *
+ * @author GOSPOGA
+ */
+public class Army {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
