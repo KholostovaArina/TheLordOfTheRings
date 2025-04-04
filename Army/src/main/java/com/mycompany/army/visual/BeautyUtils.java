14d6117c2c8be8889c7dd9a4e15e0086ac6eb28b
Холостова Арина <airsha2004@gmail.com>
2025-04-05 00:46:31 +0300

    the first attempt to set fonts and images for everyone

--- Army/src/main/java/com/mycompany/army/visual/BeautyUtils.java
+++ Army/src/main/java/com/mycompany/army/visual/BeautyUtils.java
@@ -1,36 +1,33 @@
 package com.mycompany.army.visual;
 
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.imageio.ImageIO;
 
-public class BeautyUtils {
-    private static Font customFont;
-    static{
-        try {
-            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("C:\\Users\\GOSPOGA\\OneDrive\\Рабочий стол\\lr\\tolkiencyr.ttf"));
-            customFont = customFont.deriveFont(18f);
-        } catch (IOException | FontFormatException e) {
-            customFont = new Font("Serif", Font.PLAIN, 18);
-        }
-    }
-    
-    public static Font getFont(){
-        return customFont;
-    }
-    private static Image natureImage;
-    private static Image mramorImage;
-    private static Image paperImage;   
-    static {
-        try {
-            natureImage = ImageIO.read(new File("C:\\Users\\GOSPOGA\\OneDrive\\Рабочий стол\\lr\\лето.jpg"));
-            paperImage = ImageIO.read(new File("C:\\Users\\GOSPOGA\\OneDrive\\Рабочий стол\\lr\\бумага.jpg"));
-            mramorImage = ImageIO.read(new File("C:\\Users\\GOSPOGA\\OneDrive\\Рабочий стол\\lr\\мрамор.jpg"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+   public class BeautyUtils {
+       private static Font customFont;
+       static {
+           try (InputStream fontStream = BeautyUtils.class.getResourceAsStream("/tolkiencyr.ttf")) {
+               customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+               customFont = customFont.deriveFont(18f);
+           } catch (IOException | FontFormatException e) {
+               customFont = new Font("Serif", Font.PLAIN, 18);
+           }
+       }
+
+       private static Image natureImage;
+       private static Image mramorImage;
+       private static Image paperImage;
+       static {
+           try {
+               natureImage = ImageIO.read(BeautyUtils.class.getResourceAsStream("/лето.jpg"));
+               paperImage = ImageIO.read(BeautyUtils.class.getResourceAsStream("/бумага.jpg"));
+               mramorImage = ImageIO.read(BeautyUtils.class.getResourceAsStream("/мрамор.jpg"));
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
 
     public static Image getNatureImage() {
         return natureImage;
@@ -43,13 +40,17 @@ public class BeautyUtils {
       public static Image getMramorImage() {
         return mramorImage;
     }
+      
+    public static Font getFont() {
+        return customFont;
+    }
 
     public static void setFontForAllComponents(Container container, Color color) {
         for (Component component : container.getComponents()) {
             component.setFont(customFont);
             component.setForeground(color);
-            if (component instanceof Container) {
-                setFontForAllComponents((Container) component, color);
+            if (component instanceof Container container1) {
+                setFontForAllComponents(container1, color);
             }
         }
     }
@@ -57,8 +58,8 @@ public class BeautyUtils {
     public static void setFontForAllComponents(Container container) {
         for (Component component : container.getComponents()) {
             component.setFont(customFont);
-            if (component instanceof Container) {
-                setFontForAllComponents((Container) component);
+            if (component instanceof Container container1) {
+                setFontForAllComponents(container1);
             }
         }
     }
